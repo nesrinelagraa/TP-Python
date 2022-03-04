338ec2bc2c7e9350649006f6edd843f6e409c65c
Nesrine <nesrinelagraa2@gmail.com>
2022-03-04 10:43:14 +0100

    EgyptianMult fixed

--- EgyptianMult.py
+++ EgyptianMult.py
@@ -1,3 +1,19 @@
 print('Please enter the numbers for the multiplication: ')
-number1, number2= input().split()
+n1, n2= input().split()
+n1=int(n1)
+n2=int(n2)
+base=n2
+number=n1
+if(n1 < n2): 
+    base, number=number,base
+binaryTable=list(bin(base))[2:]
+result=0
+i=0
+while i < len(binaryTable):
+    result+=number*int(binaryTable[i])*(2**(len(binaryTable)-i-1))
+    i+=1
+print(result)
+    
+
+    
 
