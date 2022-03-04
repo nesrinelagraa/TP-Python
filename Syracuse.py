19330951fcb22c11c4a29713f6e4454d26efaea4
Nesrine <nesrinelagraa2@gmail.com>
2022-03-04 09:35:11 +0100

    Syracuse.py added

---
+++ Syracuse.py
@@ -0,0 +1,11 @@
+print('Please enter a number:')
+number=int(input())
+while number > 1 :
+    if number%2==0:
+        print(number)
+        number=int(number/2)
+    else:
+        print(number)
+        number=number*3+1
+    
+
